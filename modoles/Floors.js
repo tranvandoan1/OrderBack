9ac0bd1c4f93906684904809e468296cc405d642
tranvandoan1 <tranlove24@gmail.com>
2022-07-25 16:22:44 +0700

    đá

--- modoles/Floors.js
+++ modoles/Floors.js
@@ -3,6 +3,10 @@ import mongoose from 'mongoose';
 const Floor = new mongoose.Schema({
     name: {
         type: String
-    }
+    },
+    user_id: {
+        type: ObjectId,
+        ref: "user",
+      },
 }, { timestamps: true });
 module.exports = mongoose.model('floors', Floor);
